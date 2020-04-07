560eec55eab171d03e5bab7c015995182f957732
RanjithaSrinivasa <ranjithasrinivasa24@gmail.com>
2020-04-06 21:57:48 -0400

    implementing maximum subarray sum using kadane's algorithm

---
+++ maximum-subarray-sum.hs
@@ -0,0 +1,13 @@
+maxsubarray :: [Int] -> Int
+maxsubarray = maxsubarray' 0 0
+    where
+      maxsubarray' maxsofar maxendinghere [] = maxsofar
+      maxsubarray' maxsofar maxendinghere (x:xs) = maxsubarray' maxsofar' maxendinghere' xs
+          where
+             maxendinghere' = max x (maxendinghere + x)
+             maxsofar' = max maxsofar maxendinghere'
+
+
+  
+         
+                

872c78d77bd727f3fb71f8449d466f60e3517c33
RanjithaSrinivasa <ranjithasrinivasa24@gmail.com>
2020-04-20 21:16:53 -0400

    finding nth prime

--- sortquick.hs
+++ sortquick.hs
@@ -4,3 +4,4 @@ quicksort (x:xs) = quicksort smaller ++ [x] ++ quicksort larger
                    where
                    smaller = [a | a <- xs, a <= x]
                    larger = [b | b <- xs, b > x]
+
